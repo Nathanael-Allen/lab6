0c04eb6feaa8c01afd7b9ffdecad883d22f28001
nathanaelA <nathanael.allen1@gmail.com>
2024-07-26 19:23:36 -0700

    pretty printing

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from Name import Name
 
 def main():
     names = Name.showNames()
-    print(names)
-    print('YEAR | NAME | GENDER | COUNT')
-    print('-' * 30)
+    print(f'YEAR{" " * 4}NAME{" " * 30}GENDER{" " * 8}COUNT')
+    print('_' * 62)
 
     for name in names:
-        print(f'{name.getYear()} | {name.getName()} | {name.getGender()}      | {name.getCount()}')
-        print('-' * 30)
+        print(f'{name.getYear()}{" " * 4}{name.getName()}{" " * (36 - len(name.getName()))}{name.getGender()}{" " * 11}{name.getCount()}')
 
 
 if __name__ == '__main__':
