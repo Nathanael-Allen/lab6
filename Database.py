af42cc7517a7b549663395f4060f677f967b5fca
nathanaelA <nathanael.allen1@gmail.com>
2024-07-31 17:48:13 -0700

    added comments

--- Database.py
+++ Database.py
@@ -1,4 +1,7 @@
 class Database:
+    # This is a class called Database, it's attribute __names stores a list of dictionaries
+    # the dictionaries contain dummy data of most popular names by year 
+    # between the years 1915 and 1919
     __names = [
         {"name": "John", "year": 1915, "gender": "M", "count": 47577},
         {"name": "John", "year": 1916, "gender": "M", "count": 50046},
@@ -7,6 +10,9 @@ class Database:
         {"name": "John", "year": 1919, "gender": "M", "count": 53532},
     ]
 
+    @classmethod
     def showNames(cls):
+        # This is a class method that returns the attribute __names 
+        # (a list of dictionaries) from the class Database
         return cls.__names
     
--- Name.py
+++ Name.py
@@ -1,45 +1,53 @@
 from Database import Database
 
 class Name:
+    # A class called Name with private attributes and getter and setter methods
     __name = ""
     __year = int
     __gender = ""
     __count = int
     
-
+    # Sets the name attribute
     def setName(self, name):
         self.__name = name
     
-
+    # Returns the name attribute
     def getName(self):
         return self.__name
     
-
+    # Sets the year attribute
     def setYear(self, year):
         self.__year = year
     
-
+    # Returns the year attribute
     def getYear(self):
         return self.__year
     
-
+    # sets the gender attribute 
     def setGender(self, gender):
         self.__gender = gender
     
-
+    # returns the gender attribute 
     def getGender(self):
         return self.__gender
     
-
+    # Sets the count attribute
     def setCount(self, count):
         self.__count = count
     
-
+    # returns the count attribute
     def getCount(self):
         return self.__count
     
+    
     def showNames():
-        names = Database().showNames()
+        # calls the showNames class method of the Database class, this returns a list of
+        # dictionaries. the method then loops through the list and creates a Name object
+        # it then sets the name, year,gender, and count attributes using the setter methods
+        # of the Name class. It appends this object to a list called data and once the
+        # loop is complete it returns the data list.
+        # The data list is a list of Name objects.
+        names = Database.showNames()
         data = []
         for i in range(0, len(names)):
             newName = Name()
